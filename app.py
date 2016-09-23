3e36b19ed1521d88d7afc66ac22fd97c9e7bfac4
Edwin Villanueva <edwincv0@gmail.com>
2016-09-23 14:49:01 -0700

    Started Example 2

--- app.py
+++ app.py
@@ -9,8 +9,19 @@ class HelloForm(Form):
 @app.route('/')
 
 def index():
-    return render_template('first_app.html')
+    form = HelloForm(request.form)
+    return render_template('first_app.html', form=form)
+
+@app.route('/hello', methods=['POST'])
+
+def hello():
+    form = HelloForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = request.form['sayhello']
+        return render_template('hello.html', name=name)
+    return render_template('first_app.html', form=form)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
 
